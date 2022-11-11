300811e11c3e7a0909d14bf74412f4fd5716035f
Karl Banks <karl.banksjr@gmail.com>
2022-11-11 08:21:32 -0500

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,36 +6,44 @@ import { ProductListComponent } from './product/product-list/product-list.compon
 import { ProductDetailComponent } from './product/product-detail/product-detail.component';
 import { ProductCreateComponent } from './product/product-create/product-create.component';
 import { ProductChangeComponent } from './product/product-change/product-change.component';
+
 import { RequestListComponent } from './request/request-list/request-list.component';
 import { RequestDetailComponent } from './request/request-detail/request-detail.component';
 import { RequestCreateComponent } from './request/request-create/request-create.component';
 import { RequestChangeComponent } from './request/request-change/request-change.component';
+import { RequestReviewComponent } from './request/request-review/request-review.component';
+
 import { RequestlineListComponent } from './requestline/requestline-list/requestline-list.component';
 import { RequestlineDetailComponent } from './requestline/requestline-detail/requestline-detail.component';
 import { RequestlineCreateComponent } from './requestline/requestline-create/requestline-create.component';
 import { RequestlineChangeComponent } from './requestline/requestline-change/requestline-change.component';
+
 import { UserListComponent } from './user/user-list/user-list.component';
 import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserCreateComponent } from './user/user-create/user-create.component';
 import { UserChangeComponent } from './user/user-change/user-change.component';
 import { UserLoginComponent } from './user/user-login/user-login.component';
 import { VendorListComponent } from './vendor/vendor-list/vendor-list.component';
+
 import { VendorDetailComponent } from './vendor/vendor-detail/vendor-detail.component';
 import { VendorCreateComponent } from './vendor/vendor-create/vendor-create.component';
 import { VendorChangeComponent } from './vendor/vendor-change/vendor-change.component';
+
 import { HomeComponent } from './misc/home/home.component';
 import { AboutComponent } from './misc/about/about.component';
 import { E404Component } from './misc/e404/e404.component';
-import { RequestReviewComponent } from './request/request-review/request-review.component';
 import { MenuComponent } from './misc/menu/menu.component';
+
 import { ReviewListComponent } from './requestline/review-list/review-list.component';
 import { ReviewItemComponent } from './requestline/review-item/review-item.component';
+
 
 const routes: Routes = [
   {path:"",redirectTo:"user/login",pathMatch:"full"},
 
   {path:"home",component: HomeComponent},
   {path:"about",component:AboutComponent},
+
   {path:"product",component:ProductListComponent},
   {path:"product/change/:id",component:ProductChangeComponent},
   {path:"product/create",component:ProductCreateComponent},
@@ -55,11 +63,14 @@ const routes: Routes = [
   {path:"vendor/detail/:id",component:VendorDetailComponent},
 
 
+
+
   {path:"request",component:RequestListComponent},
   {path:"request/change/:id",component:RequestChangeComponent},
   {path:"Requests/create",component:RequestCreateComponent},
   {path:"request/detail/:id",component:RequestDetailComponent},
-  {path:"review",component:RequestReviewComponent},
+  {path:"request/review",component:RequestReviewComponent},
+
 
 
   {path:"requestline/:id",component:RequestlineListComponent},
